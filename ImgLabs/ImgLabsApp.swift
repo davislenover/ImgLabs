b8891e780b7e5b2d4cafde6d89f38b4690b4a5fe
Davis Lenover <davislenover02@gmail.com>
2026-03-02 21:44:12 -0500

    Initial Commit

---
+++ ImgLabs/ImgLabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImgLabsApp.swift
+//  ImgLabs
+//
+//  Created by Davis Lenover on 2026-03-02.
+//
+
+import SwiftUI
+
+@main
+struct ImgLabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
