bec84616907bce4ff9734b17978606d6f22c9511
Davis Lenover <davislenover02@gmail.com>
2026-07-02 20:41:10 -0400

    Changed float3 type to float4 to better align with memory. Added encode function to GrayScaleConvert

--- ImgLabs/GrayScaleConvert.swift
+++ ImgLabs/GrayScaleConvert.swift
@@ -12,6 +12,11 @@ class GrayScaleConvert : ComputeKernel {
     
     private let rgbaBuf : MTLBuffer; // UChar array holding RGBA values, single photo
     private let grayScaleBuf : MTLBuffer; // Resulting UChar array after conerting to grayscale
+    private static let RED_WEIGHT : Float = 0.299;
+    private static let GREEN_WEIGHT : Float = 0.587;
+    private static let BLUE_WEIGHT : Float = 0.114;
+    private static let rgbWeights : SIMD4<Float> = .init(RED_WEIGHT, GREEN_WEIGHT, BLUE_WEIGHT, 0.0); // 0.0 so ending doesn't affect calculation
+    private static let backgroundColor : SIMD4<Float> = .init(0.0, 0.0, 0.0, 1.0);
     
     /// Creates an instance of GrayScaleConvert
     /// - Parameters:
@@ -34,7 +39,28 @@ class GrayScaleConvert : ComputeKernel {
     
     func encode() -> (any MTLComputeCommandEncoder, any MTLComputePipelineState) throws -> () {
         return ({ (encoder, pipelineState) in
-            return;
+            // Setup memory
+            var rgbWeights : SIMD4<Float> = Self.rgbWeights;
+            var bkgColor : SIMD4<Float> = Self.backgroundColor;
+            // setBytes copies small memory directly to the encoded command (i.e., it's inlined thus being fast)
+            encoder.setBytes(&rgbWeights, length: MemoryLayout.size(ofValue: rgbWeights), index: 2);
+            encoder.setBytes(&bkgColor, length: MemoryLayout.size(ofValue: bkgColor), index: 1);
+            encoder.setBuffer(self.rgbaBuf, offset: 0, index: 0);
+            encoder.setBuffer(self.grayScaleBuf, offset: 0, index: 3);
+            
+            // Setup thread dispatch
+            // Determine the total 1D grid size (total number of pixel threads to run)
+            let totalPixelThreads = self.rgbaBuf.length / MemoryLayout<UInt8>.stride;
+            let gridExecutionSize = MTLSize(width: totalPixelThreads, height: 1, depth: 1);
+
+            // Query the pipeline state to discover the absolute maximum allocation permitted by the GPU hardware
+            // This safely maximizes the threads inside the group (commonly returns 512 or 1024)
+            let maxThreadsAllowed = pipelineState.maxTotalThreadsPerThreadgroup;
+            let threadsPerGroup = MTLSize(width: maxThreadsAllowed, height: 1, depth: 1);
+
+            // Dispatch the grid setup
+            // Metal automatically breaks the grid into chunks of 'maxThreadsAllowed'
+            encoder.dispatchThreads(gridExecutionSize, threadsPerThreadgroup: threadsPerGroup);
         });
     }
     
